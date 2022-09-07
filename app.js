22a1e14a658824492abcb3a77137de0de1fa2e93
summydev <adegbitesumayah@gmail.com>
2022-09-07 10:24:25 +0100

    fruitsProject-mongoDb

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsprojects", { useNewUrlParser: true });
+// const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
-const url = "mongodb://localhost:27017";
-const dbName = "fruits";
-const client = new MongoClient(url);
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("connected tothe server");
-
-  const db = client.db(dbName);
-  findDocuments(db, function () {
-    client.close();
-  });
-});
 
 const insertDocument = function (db, callback) {
   const collection = db.collection("fruits");
